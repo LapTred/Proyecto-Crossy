41efc8dd06207eb86cb508d5029c67476453da7f
LapTred <braulio_damian@hotmail.com>
2023-03-08 19:56:21 -0800

    Initial commit

---
+++ Assets/Scripts/TerrainData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Enumeration;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Terrain Data", menuName = "Terrain Data")]
+
+public class TerrainData : ScriptableObject
+{
+    public GameObject terrain;
+    public int maxInSuccesion;
+
+
+
+}
